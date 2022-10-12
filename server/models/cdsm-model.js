996b3c5a6f82ad75642a063e95e2ad5688a1bf8b
Cyfurion <patrick.fan@stonybrook.edu>
2022-10-12 02:59:50 -0400

    Fix cdsm-model.js

--- server/models/cdsm-model.js
+++ server/models/cdsm-model.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
-const AccessControlRequirement = require("../classes/accesscontrolrequirement-class");
-const FileSnapshot = require("../classes/filesnapshot-class");
-const GroupSnapshot = require("../classes/groupsnapshot-class");
 const Schema = mongoose.Schema;
 
 const CDSMSchema = new Schema(
     {
         id: { type: [String], required: true },
-        acrs: { type: [AccessControlRequirement], required: false },
-        filesnapshots: { type: [FileSnapshot], required: true },
-        groupsnapshots: { type: [GroupSnapshot], required: false }
+        acrs: { type: [String], required: false },
+        filesnapshots: { type: [String], required: true },
+        groupsnapshots: { type: [String], required: false }
     }
 );
 
