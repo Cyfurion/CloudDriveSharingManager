e03556e51890e12acb72494f61336c3fa186b967
Meako-Chan <michael.w.chan@stonybrook.edu>
2022-10-13 02:32:41 -0400

    added CSS for Logo and TopBar

---
+++ client/src/components/TopBar.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import AppLogo from './AppLogo';
+
+function TopBar() {
+
+  return (
+    <AppBar position="static" style={{background:'#FFFFFF'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AppLogo />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default TopBar;
