54698f3d2bcfcb3e19d9902331d351c8a198f0e6
Lexi Jeong <lexijeong@Lexis-MacBook-Air.local>
2022-10-14 22:00:33 -0400

    dropbox snapshot

--- client/src/cloudservices/DropboxCloudServiceAdapter.js
+++ client/src/cloudservices/DropboxCloudServiceAdapter.js
@@ -1,5 +1,6 @@
+import { Folder } from "../classes/file-class";
 import { CloudServiceAdapter } from "./CloudServiceAdapter";
-
+import { FolderMetadata } from "dropbox";
 export class DropboxCloudServiceAdapter extends CloudServiceAdapter {
     deploy() {
         //TODO not implemented
@@ -7,6 +8,7 @@ export class DropboxCloudServiceAdapter extends CloudServiceAdapter {
 
     // Returns an array of every file accessible to the user.
     async retrieve() {
+
         let files = [];
         let response = await this.endpoint.filesListFolder({ 
             path: '',
@@ -20,6 +22,27 @@ export class DropboxCloudServiceAdapter extends CloudServiceAdapter {
                 createdTime: element.client_modified
             });
         }
+
+        let root = new Folder("root", "root", "root", []);
+        await makeSnapshot(root, '', this.endpoint);
+        console.log(root);
         return files;
+    }
+}
+
+async function makeSnapshot(folder, path, endpoint){
+    console.log("path: ", path);
+    let response = await endpoint.filesListFolder({ 
+        path: path
+    });
+    for (let element of response.result.entries) {
+        if(element[".tag"] === "folder"){
+            let newFolder = new Folder(["dropBoxNoId"], element.name, [], []);
+            await makeSnapshot(newFolder, path + "/" + element.name, endpoint);
+            folder.files.push(newFolder);
+        }else{
+            let newFile = new File(["dropBoxNoId"], element.name, []);
+            folder.files.push(newFile);
+        }
     }
 }
