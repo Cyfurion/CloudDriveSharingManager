43183537796bbd65d34d5c264fa538fba3d76d4e
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 <moinul.ahsan@stonybrook.edu>
2022-10-29 05:46:02 -0400

    change font for displaying files, added underline under folder names for clarification on what can be clicked

--- client/src/components/WorkSpace.js
+++ client/src/components/WorkSpace.js
@@ -10,22 +10,22 @@ export default function WorkSpace( props ) {
             <table >
                 <thead className="border-b-2 border-gray-200">
                     <tr className="filecard ">
-                        <th > Name </th>
+                        <th class="pl-6"> Name </th>
                         <th > Owner </th>
                         <th > Date Created </th>
                     </tr>
                 </thead>
                 <tbody >
                     {props.data.map((file) => (
-                        <tr id={file.id} onClick={ file.files === undefined ? null  : (e) => props.handleClickFolder(e, file)} className="filecard border-b-2 hover:bg-gray-100 ">
-                            <td className='max-w-[40vw] min-w-[40vw] text-ellipsis overflow-hidden whitespace-nowrap' >{ typeof file.files === 'undefined' ? <InsertDriveFileIcon /> : <FolderIcon />  }{file.name} </td>
+                        <tr id={file.id}  className="filecard border-b-2 hover:bg-gray-100">
+                            <td className='max-w-[40vw] min-w-[40vw] text-ellipsis overflow-hidden whitespace-nowrap' >{ file.files === undefined ? <InsertDriveFileIcon /> : <FolderIcon />  }
+                            <span  className={"" + (file.files !== undefined ? "underline" : "")} onClick={ file.files === undefined ? null  : (e) => props.handleClickFolder(e, file)} >{file.name}</span> </td>
                             <td className='min-w-[25vw] max-w-[25vw] whitespace-nowrap '> {file.owner} </td>
                             <td className='min-w-[20vw] max-w-[20vw] whitespace-nowrap '>  {file.createdTime} </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-
         );
     }
 }
