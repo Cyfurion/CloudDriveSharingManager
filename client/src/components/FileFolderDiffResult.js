3f287e9c6eb549e5e61ccf669b075c0f30d9a2ea
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 <moinul.ahsan@stonybrook.edu>
2022-11-01 21:19:23 -0400

    show file foler diff results on modal

--- client/src/components/FileFolderDiffResult.js
+++ client/src/components/FileFolderDiffResult.js
@@ -18,11 +18,36 @@ export default function FileFolderDiffResult(props) {
                             <svg aria-hidden="true" className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             <span className="sr-only">Close modal</span>
                         </button>
+                    </div>
 
+                    <div className="flex flex-col p-4 ">
+                        <div className="border-b border-black "> Current Folder: {props.result.folder.name} </div>
+
+                        <h1 className="border-b"> Differing Files: </h1>
+                        <div className="flex accordion flex-col max-h-64 overflow-y-scroll  ">
+                            {props.result.differingFiles.map((file)=>(
+                                <div className="border-b pl-5"> File Name: {file.file.name}
+                                    
+                                    {file.fileDifferences.map((permission)=>(
+                                        <div className="flex flex-col pl-5 bg-green-100">
+                                            <h1> Entity: {permission.entity}</h1>
+                                            <h1> Role: {permission.role}</h1>
+                                         </div>
+                                    ))}
+                                    {file.folderDifferences.map((permission)=>(
+                                        <div className="flex flex-col pl-5">
+                                            folder Differences:
+                                            <h1> Entity: {permission.entity}</h1>
+                                            <h1> Role: {permission.role}</h1>
+                                         </div>
+                                    ))}
+                                 </div>
+                                
+                            ))}
+                        </div>
 
                     </div>
 
-                   
                 </div>
             </div>
         </div>
