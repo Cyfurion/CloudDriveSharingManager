3eed02762972385d2453a68d502bbe458831d204
Lexi Jeong <lexijeong@Lexis-MacBook-Air.local>
2022-11-24 02:36:48 -0500

    fixed analysis errors due to changes to permission fields, restructured reutrn of deviant analysis, added (untested) compareSnapshots

--- client/src/classes/permission-class.js
+++ client/src/classes/permission-class.js
@@ -5,6 +5,15 @@ class Permission {
         this.role = role; // read, write, comment
         this.isInherited = isInherited;
         this.canShare = canShare;
+        this.basePermission = new BasePermission(type, entity, role);
+    }
+}
+
+class BasePermission{
+    constructor(type, entity, role){
+        this.type = type; // user, group, domain
+        this.entity = entity;// associated domain or emails
+        this.role = role; // read, write, comment
     }
 }
 
